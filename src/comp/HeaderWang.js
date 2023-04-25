d80f0b82e7323a2fb9eb91f369303a956c7f8b78
Yushan Wang <yushanwang0816@gmail.com>
2023-04-25 10:27:50 +0800

    header responsiveness fixed

--- src/comp/HeaderWang.js
+++ src/comp/HeaderWang.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import { useRef } from "react";
+import { useState } from 'react';
+import { FaBars, FaTimes } from "react-icons/fa";
 import "./../css/HeaderWang.css";
 
 const HeaderWang = ({switchTheme, theme}) => {
+    const navRef = useRef();
+    const [isOpen, setIsOpen] = useState(false);
+
+    const showNavbar = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+        setIsOpen(!isOpen);
+	};
+
     return (
         <div className="HeaderWang">
             <a href="/"><h1>YushanWang9801</h1> </a>   
-            <nav className="navbar">
-                <a href="/#/project">Projects</a>
-                <a href="/#/blog">Blogs</a>
-                <a href="/#/gallery">Gallery</a>
-                <a href="/#/me" id="Me">Me</a>
+            <nav className="navbar" ref={navRef}>
+                <a href="/#/project" onClick={showNavbar}>Projects</a>
+                <a href="/#/blog"    onClick={showNavbar}>Blogs</a>
+                <a href="/#/gallery" onClick={showNavbar}>Gallery</a>
+                <a href="/#/me" id="Me"
+                                     onClick={showNavbar}>Me</a>
             </nav>
             <div className='dark_mode'>
                 <input
@@ -20,6 +33,11 @@ const HeaderWang = ({switchTheme, theme}) => {
                     onChange={switchTheme}
                 />
             </div>
+            <button
+				className="nav-btn"
+				onClick={showNavbar}>
+				{isOpen ? <FaTimes /> : <FaBars />}
+			</button>
         </div>
     );
 }
